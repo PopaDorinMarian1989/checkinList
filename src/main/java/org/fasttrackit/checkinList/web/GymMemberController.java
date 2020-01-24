373905c6519d690b6f9cdf397ad2c9b6eec413b5
popa dorin <dorinmarian89@gmail.com>
2020-01-24 21:49:02 +0200

    added something

--- src/main/java/org/fasttrackit/checkinList/web/GymMemberController.java
+++ src/main/java/org/fasttrackit/checkinList/web/GymMemberController.java
@@ -1,122 +1,32 @@
 package org.fasttrackit.checkinList.web;
 
 import org.fasttrackit.checkinList.dto.GymMemberDTO;
-import org.fasttrackit.checkinList.service.CheckinListService;
-import org.fasttrackit.checkinList.service.ConvertorUtils;
+import org.fasttrackit.checkinList.model.GymMember;
 import org.fasttrackit.checkinList.service.GymMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/gymMember")
 public class GymMemberController {
 
-    // private final CheckinListService checkinListService;
 
-    /// @Autowired
-    //  public GymMemberController(CheckinListService checkinListService) {
-    //    this.checkinListService = checkinListService;
-    // }
-
-    // @GetMapping
-    //  public List<GymMemberDTO> getGymMembersByCheckinLists(@PathVariable long checkinListId) {
-
-    //    return checkinListService.getCheckinList(checkinListId).getGymMembers();
-    //  }
-
-    //@RequestMapping(path = "/check-in-list/{checkinListId}/gmMember", method = RequestMethod.GET)
-    // @ResponseBody
-    //public List<GymMemberDTO> getGymMembersByCheckinLists(@PathVariable long checkinListId) {
-
-    //   return checkinListService.getCheckinList(checkinListId).getGymMembers();
-    // }
-
-    //  @PutMapping
-    // public GymMemberDTO editPassport(@PathVariable long checkinListId, @PathVariable long gymMmemberId,
-    //                                @RequestBody GymMemberDTO request) {
-    //   request.setId(gymMmemberId);
-    //   GymMember gymMmember = new GymMember();
-    //   gymMmember.update(request);
-    //   return request;}
-    // @RequestMapping(path = "/check-in-list/{checkinListId}/gymMember/{gymMemberId}", method = RequestMethod.PUT)
-    // @ResponseBody
-    //  public GymMemberDTO editPassport(@PathVariable long checkinListId, @PathVariable long gymMmemberId,
-    //                              @RequestBody GymMemberDTO request) {
-    //  request.setId(gymMmemberId);
-    //  gymMmember.update(request);
-    //  return request;
-    // }
-//@PostMapping
-//public <GymMemberService> GymMemberDTO saveGymMember (@PathVariable long checkinListId,
-    //                                                  @RequestBody GymMemberDTO request){
-
-
-    // GymMemberService gymMemberService = new GymMemberService();
-    // gymMemberService.create(request, checkinListId);
-    //return request;
-//}
-
-    // @RequestMapping(path = "/check-in-list/{checkinListId}/gymMember", method = RequestMethod.POST)
-    // @ResponseBody
-    // public GymMemberDTO saveGymMember ( @PathVariable long checkinListId,
-    // @RequestBody GymMemberDTO request){
-
-    //    gymMemberService.create(request, checkinListId);
-    //    return request;
-    //  }
-
-//@DeleteMapping
-
-    //   @RequestMapping(path = "/check-in-list/gymMmember/{gymMmeberId}", method = RequestMethod.DELETE)
-
-    //public void deleteGymMmember ( @PathVariable long gymMmemberId){
-
-    //  gymMemberService.delete(gymMmemberId);
-
-    //   }
-    //  }
+    private final GymMemberService gymMemberService;
 
     @Autowired
-    private CheckinListService service;
-    @Autowired
-    private GymMemberService gymMemberService;
-
-    @RequestMapping(path = "/check-in-list/{checkinListId}/gymMember", method = RequestMethod.GET)
-    @ResponseBody
-    public List<GymMemberDTO> getGymMembersByCheckinLists(@PathVariable long checkinListId) {
-
-        return service.getCheckinList(checkinListId).getGymMembers();
+    public GymMemberController(GymMemberService gymMemberService) {
+        this.gymMemberService = gymMemberService;
     }
 
-
-    @CrossOrigin
-    @RequestMapping(path = "/check-in-list/{checkinListId}/gymMember/{gymMemberId}", method = RequestMethod.PUT)
-    @ResponseBody
-    public GymMemberDTO editGymMember(@PathVariable long checkinListId, @PathVariable long gymMemberId,
-                                      @RequestBody GymMemberDTO request) {
-        request.setId(gymMemberId);
-        gymMemberService.update(request);
-        return request;
-    }
-
-    @RequestMapping(path = "/check-in-list/{checkinListId}/gymMember", method = RequestMethod.POST)
-    @ResponseBody
-    public GymMemberDTO saveGymMember(@PathVariable long checkinListId,
-                                      @RequestBody GymMemberDTO request) {
-
-        gymMemberService.create(request, checkinListId);
-        return request;
-    }
-
-
-    @CrossOrigin
-    @RequestMapping(path = "/check-in-list/gymMember/{gymMemberId}", method = RequestMethod.DELETE)
-
-    public void deleteGymMember(@PathVariable long gymMemberId) {
-
-        gymMemberService.delete(gymMemberId);
-
+    @PostMapping
+    public ResponseEntity<GymMember> createGymMember(@RequestBody @Valid GymMemberDTO request){
+        GymMember gymMember = gymMemberService.create(request);
+        return new ResponseEntity<>(gymMember, HttpStatus.OK);
     }
 }
--- src/test/java/org/fasttrackit/checkinList/CheckinListRepoTest.java
+++ src/test/java/org/fasttrackit/checkinList/CheckinListRepoTest.java
@@ -1,9 +1,6 @@
 package org.fasttrackit.checkinList;
 
-import org.fasttrackit.checkinList.model.CheckinList;
 import org.fasttrackit.checkinList.model.GymMember;
-import org.fasttrackit.checkinList.repo.CheckinListRepository;
-import org.fasttrackit.checkinList.service.CheckinListService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,83 +16,83 @@ import java.util.List;
       //  loader = CheckinListRepoTest.SpringApplicationContextLoader.class,
      //   initializers = ConfigFileApplicationContextInitializer.class)
 
-public class CheckinListRepoTest {
-    @Autowired
-    private CheckinListRepository repository;
-
-    @Autowired
-    private CheckinListService service;
-
-
-    @Test
-    public void testSave() {
-
-        CheckinList checkinList = getCheckinList("checkinList1", "firtsname", "ln", "0");
-        repository.save(checkinList);
-
-
-        CheckinList checkinList2 = getCheckinList("checkinList2", "Popa", "Dorin", "1");
-        repository.save(checkinList2);
-
-
-    }
-
-
-    @Test
-    public void testFindByName() {
-
-        List<CheckinList> checkinList1List = repository.findByName("checkinList1");
-        System.out.println(checkinList1List.size());
-
-        System.out.println("-----------------------REZULTAT TEST");
-        System.out.println(checkinList1List.get(0));
-
-
-        List<CheckinList> checkinListLikeList = repository.findByNameContaining("agenda");
-        System.out.println(checkinListLikeList.size());
-
-        System.out.println("-----------------------REZULTAT TEST LIKE");
-
-        for (CheckinList checkinList : checkinListLikeList) {
-            System.out.println(checkinList);
-        }
-
-    }
-
-
-    private CheckinList getCheckinList(String checkinListName, String firtsname, String lastName, String gymMemberNumber) {
-        CheckinList checkinList = new CheckinList();
-        checkinList.setName(checkinListName);
-        List<GymMember> gymMembers = new ArrayList<>();
-        GymMember gymMember = new GymMember();
-        gymMember.setFirstName(firtsname);
-        gymMember.setLastName(lastName);
-        gymMember.setGymMembernumber(gymMemberNumber);
-        gymMembers.add(gymMember);
-        checkinList.setGymMembers(gymMembers);
-        return checkinList;
-    }
-
-    public CheckinListService getService() {
-        return service;
-    }
-
-    public void setService(CheckinListService service) {
-        this.service = service;
-    }
-
-    //  static class SpringApplicationContextLoader implements ContextLoader {
-     //   @Override
-      //  public String[] processLocations(Class<?> aClass, String... strings) {
-       //     return new String[0];
-       // }
-
-       // @Override
-      //  public ApplicationContext loadContext(String... strings) throws Exception {
-        //    return null;
-      //  }
-  //  }
-
-   // class CheckinlistApplication {
-    }
-
+public class CheckinListRepoTest {}
+//    @Autowired
+//    private CheckinListRepository repository;
+//
+//    @Autowired
+//    private CheckinListService service;
+//
+//
+//    @Test
+//    public void testSave() {
+//
+//        CheckinList checkinList = getCheckinList("checkinList1", "firtsname", "ln", "0");
+//        repository.save(checkinList);
+//
+//
+//        CheckinList checkinList2 = getCheckinList("checkinList2", "Popa", "Dorin", "1");
+//        repository.save(checkinList2);
+//
+//
+//    }
+//
+//
+//    @Test
+//    public void testFindByName() {
+//
+//        List<CheckinList> checkinList1List = repository.findByName("checkinList1");
+//        System.out.println(checkinList1List.size());
+//
+//        System.out.println("-----------------------REZULTAT TEST");
+//        System.out.println(checkinList1List.get(0));
+//
+//
+//        List<CheckinList> checkinListLikeList = repository.findByNameContaining("agenda");
+//        System.out.println(checkinListLikeList.size());
+//
+//        System.out.println("-----------------------REZULTAT TEST LIKE");
+//
+//        for (CheckinList checkinList : checkinListLikeList) {
+//            System.out.println(checkinList);
+//        }
+//
+//    }
+//
+//
+//    private CheckinList getCheckinList(String checkinListName, String firtsname, String lastName, String gymMemberNumber) {
+//        CheckinList checkinList = new CheckinList();
+//        checkinList.setName(checkinListName);
+//        List<GymMember> gymMembers = new ArrayList<>();
+//        GymMember gymMember = new GymMember();
+//        gymMember.setFirstName(firtsname);
+//        gymMember.setLastName(lastName);
+//        gymMember.setGymMembernumber(gymMemberNumber);
+//        gymMembers.add(gymMember);
+//        checkinList.setGymMembers(gymMembers);
+//        return checkinList;
+//    }
+//
+//    public CheckinListService getService() {
+//        return service;
+//    }
+//
+//    public void setService(CheckinListService service) {
+//        this.service = service;
+//    }
+//
+//    //  static class SpringApplicationContextLoader implements ContextLoader {
+//     //   @Override
+//      //  public String[] processLocations(Class<?> aClass, String... strings) {
+//       //     return new String[0];
+//       // }
+//
+//       // @Override
+//      //  public ApplicationContext loadContext(String... strings) throws Exception {
+//        //    return null;
+//      //  }
+//  //  }
+//
+//   // class CheckinlistApplication {
+//    }
+//
